55a4f7798682a8993bf018f4dfa0e886880fa2b3
Jan Jankowski <dwuj4n@gmail.com>
2026-02-07 21:34:39 +0100

    some work

---
+++ packages/db/src/config/index.ts
@@ -0,0 +1,41 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+import { Client, Pool } from "pg";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL!,
+});
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const db = drizzle({
+    client: pool,
+    casing: "snake_case",
+});
+
+export async function runMigrations() {
+    try {
+        console.log("Running migrations...");
+        const migrationDb = drizzle({
+            client,
+            casing: "snake_case",
+        });
+        await migrate(migrationDb, {
+            migrationsFolder: "./drizzle",
+        });
+
+    } catch (error) {
+        console.error("Migration failed:", error);
+        throw(error);
+    }
+}
---
+++ packages/db/src/schema/businesstrips.ts
@@ -0,0 +1,75 @@
+import { AnyPgColumn } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable as table } from "drizzle-orm/pg-core";
+import * as t from "drizzle-orm/pg-core";
+import { relations } from 'drizzle-orm';
+import { timestamps } from "./common";
+//import { requestStatus } from "../types/request";
+
+const requestStatusNames = [
+  "draft",
+  "toApprove",
+  "cancelled",
+  "approved",
+  "rejected",
+  "toCompleting",
+  "completed",
+  "inProgress",
+] as const;
+
+type RequestStatus = (typeof requestStatusNames)[number];
+
+export const requestStatusEnum = pgEnum("request_statuses", requestStatusNames);
+
+export const userTable = table("user", {
+  id: t.serial().primaryKey(),
+  email: t.varchar({ length: 128 }).notNull().unique(),
+  ...timestamps,
+});
+
+export const userRelations = relations(userTable, ({ many }) => ({
+  posts: many(requestTable),
+}));
+
+export const cityTable = table("city", {
+  id: t.serial().primaryKey(),
+  name: t.varchar({ length: 128 }).notNull(),
+  voivodeship: t.smallint().notNull(),
+  ...timestamps,
+});
+
+export const cityRelations = relations(cityTable, ({ many }) => ({
+  posts: many(requestTable),
+}));
+
+export const requestTable = table("request", {
+  id: t.serial().primaryKey(),
+  beneficiaryId: t
+    .integer()
+    .references((): AnyPgColumn => userTable.id)
+    .notNull(),
+  cityFromId: t
+    .integer()
+    .references((): AnyPgColumn => cityTable.id)
+    .notNull(),
+  cityToId: t
+    .integer()
+    .references((): AnyPgColumn => cityTable.id),
+  dateFrom: t.date(),
+  dateTo: t.date(),
+  status: requestStatusEnum().default("draft"),
+});
+
+export const requestRelations = relations(requestTable, ({ one }) => ({
+	beneficiary: one(userTable, {
+		fields: [requestTable.beneficiaryId],
+		references: [userTable.id],
+	}),
+    cityFrom: one(cityTable, {
+        fields: [requestTable.cityFromId],
+        references: [cityTable.id],
+    }),
+    cityTo: one(cityTable, {
+        fields: [requestTable.cityToId],
+        references: [cityTable.id],
+    }),
+}));
